81f4417468f083cc2eca93916da205a3313bb243
marksikaundi <emmanuelmark117@gmail.com>
2022-01-19 17:45:04 +0200

    grade graph

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -32,7 +32,7 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    labels: ["Vera", "Emmanuel", "Steven", "Billy", "Ndala", "Mwala", "Lecture"],
     datasets: [{
       label: "Earnings",
       lineTension: 0.3,
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [60, 10, 0, 70, 80, 50, 100],
     }],
   },
   options: {
@@ -78,7 +78,7 @@ var myLineChart = new Chart(ctx, {
           padding: 10,
           // Include a dollar sign in the ticks
           callback: function(value, index, values) {
-            return '$' + number_format(value);
+            return '%' + number_format(value);
           }
         },
         gridLines: {
